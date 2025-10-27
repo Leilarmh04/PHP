f0feb13fb8e48de08f0c27426d9e2c38105cb79f
leilarmh04 <rmhleila@gmail.com>
2025-10-27 15:54:29 +0100

    exophp

---
+++ affichage.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "<p>Bonjour tout le monde !</p>";
+
+        echo "<br>";
+
+        print "<p>Aujourd'hui, nous sommes le 27/10/2025.</p>";
+
+        echo "<br>";
+
+
+
+    ?>
+    
+</body>
+</html>
---
+++ html_php.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <div>
+        <h1>Je suis entrain d'apprendre le PHP et je commence a comprendre</h1>
+    </div>
+
+    <?php
+        echo "<p>Ce texte est affiché depuis PHP</p>";
+    ?>
+    
+</body>
+</html>
